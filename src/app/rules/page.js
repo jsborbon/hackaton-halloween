4cdc43a235e2bc73a7cbf134a80880133d5cafdf
beamm536 <beamm023@gmail.com>
2024-10-30 20:52:51 +0100

    creacion del efecto del typing y arreglar la posicion de los botones

--- src/app/rules/page.js
+++ src/app/rules/page.js
@@ -10,23 +10,29 @@ export default function Rules() {
       <div className={styles.caja}>
         <h3>Objetivos del Juego:</h3>
         <p>
-        Avanzar a través de seis habitaciones, 
-        resolviendo acertijos para alcanzar la libertad. 
-        Cada cuarto encierra un reto; resolverlo correctamente 
-        abre la puerta hacia el siguiente, mientras que un error 
-        puede retrasar o incluso detener tu progreso. 
-        
-        Piensa con cuidado: el juego pone a prueba tu ingenio y tu juicio.
+          Avanzar a través de seis habitaciones, 
+          resolviendo acertijos para alcanzar la libertad. 
+          Cada cuarto encierra un reto; resolverlo correctamente 
+          abre la puerta hacia el siguiente, mientras que un error 
+          puede retrasar o incluso detener tu progreso. 
+          
+          Piensa con cuidado: el juego pone a prueba tu ingenio y tu juicio.
         </p>
-      </div>
 
-      <div>
         <button
           type="button"
           onClick={() => router.back()}
           className={styles.btnNextPage}
         >
-          IniciO 👻
+          Demasiado miedo, mejor me vuelvo 😱
+        </button>
+
+        <button
+          type="button"
+          onClick={() => router.back()}
+          className={styles.btnNextPage}
+        >
+          Continuamos.. ➡️
         </button>
       </div>
     </div>
