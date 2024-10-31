fe4f06bef829ac55a55d54b5e5535bbd880dea50
beamm536 <beamm023@gmail.com>
2024-10-31 12:53:21 +0100

    acertijo 3 _ eleccion de las puertas

--- src/app/acertijo-3/page.js
+++ src/app/acertijo-3/page.js
@@ -1,10 +1,27 @@
-import CajaMusical from "./CajaMusical";
+"use client"; // Asegúrate de incluir esta línea para indicar que el componente es del lado del cliente
+
+import styles from "./acertijo3.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Acertijo3() {
+  const router = useRouter();
+
+  const handleClickIzquierda = () => {
+    router.push("/susto");
+  };
+  
+  const handleClickDerecha = () => {
+    router.push("/acertijo-2");
+  };
+
   return (
-    <div>
-      <h1>Caja Musical de Terror</h1>
-      <CajaMusical />
+    <div className={styles.fondoPuertas}>
+      <div className={styles.puertaIzquierda} onClick={handleClickIzquierda}>
+        <h2>PRIMERA PUERTA</h2>
+      </div>
+      <div className={styles.puertaDerecha} onClick={handleClickDerecha}>
+        <h2>SEGUNDA PUERTA</h2>
+      </div>
     </div>
   );
 }
