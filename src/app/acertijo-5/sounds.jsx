f63bc65f2a8345a266b610b58da446b35e351b27
jsborbon <santiagoborbon@gmail.com>
2024-10-31 13:07:42 +0100

    acertijo 5

---
+++ src/app/acertijo-5/sounds.jsx
@@ -0,0 +1,102 @@
+const sounds = {
+  C3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/C3.mp3")
+      : undefined,
+  D3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/D3.mp3")
+      : undefined,
+  E3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/E3.mp3")
+      : undefined,
+  F3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/F3.mp3")
+      : undefined,
+  G3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/G3.mp3")
+      : undefined,
+  A3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/A3.mp3")
+      : undefined,
+  B3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/B3.mp3")
+      : undefined,
+  C4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/C4.mp3")
+      : undefined,
+  D4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/D4.mp3")
+      : undefined,
+  E4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/E4.mp3")
+      : undefined,
+  F4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/F4.mp3")
+      : undefined,
+  G4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/G4.mp3")
+      : undefined,
+  A4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/A4.mp3")
+      : undefined,
+  B4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/B4.mp3")
+      : undefined,
+
+  DB3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/DB3.mp3")
+      : undefined,
+  EB3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/EB3.mp3")
+      : undefined,
+  GB3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/GB3.mp3")
+      : undefined,
+  AB3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/AB3.mp3")
+      : undefined,
+  BB3:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/BB3.mp3")
+      : undefined,
+
+  DB4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/DB4.mp3")
+      : undefined,
+  EB4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/EB4.mp3")
+      : undefined,
+  GB4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/GB4.mp3")
+      : undefined,
+  AB4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/AB4.mp3")
+      : undefined,
+  BB4:
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/piano/BB4.mp3")
+      : undefined,
+};
+
+export default sounds;
